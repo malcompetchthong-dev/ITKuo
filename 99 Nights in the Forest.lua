65ca95e940032cc3e668ee8cb4ddac1730988251
malcompetchthong-dev <malcom.petchthong@gmail.com>
2026-04-20 00:43:18 +0700

    Update window creation and delay for character health

--- 99 Nights in the Forest.lua
+++ 99 Nights in the Forest.lua
@@ -1,5 +1,9 @@
 local Window = loadstring(game:HttpGet("https://raw.githubusercontent.com/malcompetchthong-dev/ITKuo/refs/heads/main/KUOHUBUI.lua"))()
 
+Window:MakeWindow({
+Title = "Kuo Hub [99Nights]",
+})
+
 Window:AddMinimizeButton({
 Button = { Image = "rbxassetid://103308551113442", BackgroundTransparency = 0 },
 Corner = { CornerRadius = UDim.new(35, 1) },
@@ -176,7 +180,7 @@ function ActivateInfiniteHealth(character)
 end
 
 lp.CharacterAdded:Connect(function(char)
-    task.wait(0.3)
+    task.wait(0.5)
     if infiniteHealthEnabled then
         ActivateInfiniteHealth(char)
     end

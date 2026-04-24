d0e686da51efbe7bcad18efa4efe3ad4dab0f5d3
malcompetchthong-dev <malcom.petchthong@gmail.com>
2026-04-24 16:13:22 +0700

    Update image assets in Blockfoot.lua

--- Blockfoot.lua
+++ Blockfoot.lua
@@ -1,7 +1,7 @@
--local Window = loadstring(game:HttpGet("https://raw.githubusercontent.com/malcompetchthong-dev/ITKuo/refs/heads/main/KUOHUBUI.lua"))()
+ local Window = loadstring(game:HttpGet("https://raw.githubusercontent.com/malcompetchthong-dev/ITKuo/refs/heads/main/KUOHUBUI.lua"))()
 
 Window:AddMinimizeButton({
-    Button = { Image = "rbxassetid://103308551113442", BackgroundTransparency = 0 },
+    Button = { Image = "rbxassetid://126460540157931", BackgroundTransparency = 0 },
     Corner = { CornerRadius = UDim.new(35, 1) },
 })
 
@@ -24,7 +24,7 @@ Home:Section("Main")
 Home:AddDiscordInvite({
     Name = "Kuo Hub",
     Description = "Join server",
-    Logo = "rbxassetid://103308551113442",
+    Logo = "rbxassetid://126460540157931",
     Invite = "https://discord.gg/Apn2j9Fez",
 })
 

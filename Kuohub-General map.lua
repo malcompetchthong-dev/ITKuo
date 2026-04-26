87e0483cc73f53645faf68d336d5c9997b1b7a98
malcompetchthong-dev <malcom.petchthong@gmail.com>
2026-04-26 15:27:54 +0700

    Update button and logo assets in Kuohub map

--- Kuohub-General map.lua
+++ Kuohub-General map.lua
@@ -1,7 +1,7 @@
 local Window = loadstring(game:HttpGet("https://raw.githubusercontent.com/malcompetchthong-dev/ITKuo/refs/heads/main/KUOHUBUI.lua"))()
 
 Window:AddMinimizeButton({
-Button = { Image = "rbxassetid://103308551113442", BackgroundTransparency = 0 },
+Button = { Image = "rbxassetid://126460540157931", BackgroundTransparency = 0 },
 Corner = { CornerRadius = UDim.new(35, 1) },
 })
 
@@ -180,7 +180,7 @@ end)
 Home:AddDiscordInvite({
 Name = "Kuo Hub",
 Description = "Join server",
-Logo = "rbxassetid://103308551113442",
+Logo = "rbxassetid://126460540157931",
 Invite = "https://discord.gg/Apn2j9Fez",
 })
 
